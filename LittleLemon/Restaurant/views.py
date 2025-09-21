cec667dae454251c8c2d37c965e5854953c646cb
CodeTemka <codetemuulen@gmail.com>
2025-09-22 01:40:01 +0800

    nocomment

--- LittleLemon/Restaurant/views.py
+++ LittleLemon/Restaurant/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
+from .models import Menu, Booking
+from .serializers import menuSerializer, bookingSerializer
 # Create your views here.
