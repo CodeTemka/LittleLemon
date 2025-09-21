082b9c2f51b4113ccf92f0ab9a8d9154403e6739
CodeTemka <codetemuulen@gmail.com>
2025-09-22 00:26:39 +0800

    test1

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -74,21 +74,17 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {  
-    'mydb': {  
+    'default': {  
         'ENGINE': 'django.db.backends.mysql',  
         'NAME': 'LittleLemon',  
-        'USER': 'root',  
+        'USER': 'django',  
         'PASSWORD': 'your_password',  
         'HOST': '127.0.0.1',  
         'PORT': '3306',  
         'OPTIONS': {  
-            'init_command': "SET sql_mode='STATIC_TRANS_TABLES'"  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
         }  
-    },
-    'default': { 
-        'ENGINE': 'django.db.backends.sqlite3', 
-        'NAME': BASE_DIR / 'db.sqlite3', 
-    },
+    }
 }
 
 
--- LittleLemon/Restaurant/models.py
+++ LittleLemon/Restaurant/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField(max_digits=10, decimal_places=2)
+    Inventory = models.IntegerField()
