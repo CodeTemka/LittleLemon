adb94a7edf4d5b955e4c9acddef175d227c89bd3
CodeTemka <codetemuulen@gmail.com>
2025-09-22 01:28:44 +0800

    nocomment

--- LittleLemon/Restaurant/models.py
+++ LittleLemon/Restaurant/models.py
@@ -5,3 +5,8 @@ class Menu(models.Model):
     Title = models.CharField(max_length=255)
     Price = models.DecimalField(max_digits=10, decimal_places=2)
     Inventory = models.IntegerField()
+
+class Booking(models.Model):
+    Name = models.CharField(max_length=255)
+    No_of_guests = models.IntegerField()
+    BookingDate = models.DateField()
